f551ce908f0dc9a913f6075f8a0bc44788b66639
unknown <juaco.enero9@gmail.com>
2024-03-13 15:48:35 -0300

    renderr

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function saveTokenToServer(token) {
     const data = { token: token };
 
     // Realizar una solicitud HTTP POST al servidor
-    fetch('https://back-notificaciones-m7kp.onrender.com/save-token', {
+    fetch('https://back-notificaciones-7qk2.onrender.com/save-token', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
